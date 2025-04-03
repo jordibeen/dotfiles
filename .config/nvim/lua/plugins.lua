0be4aadd4ff22dace39c6bfa448aa4fef7a164d7
Jordi Been <jordi.been@ridecheck.app>
2025-04-03 22:33:21 +0100

    fully migrate to blink

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -31,54 +31,38 @@ local plugins = {
     -- Autocompletion framework
     {
         'saghen/blink.cmp',
-        -- optional: provides snippets for the snippet source
         dependencies = { 'rafamadriz/friendly-snippets' },
-
-        -- use a release tag to download pre-built binaries
         version = '1.*',
-        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        -- If you use nix, you can build from source using latest nightly rust with:
-        -- build = 'nix run .#build-plugin',
-
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
         opts = {
-            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-            -- 'super-tab' for mappings similar to vscode (tab to accept)
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- All presets have the following mappings:
-            -- C-space: Open menu or open docs if already open
-            -- C-n/C-p or Up/Down: Select next/previous item
-            -- C-e: Hide menu
-            -- C-k: Toggle signature help (if signature.enabled = true)
-            --
-            -- See :h blink-cmp-config-keymap for defining your own keymap
-            keymap = { preset = 'default' },
-
-            appearance = {
-                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                -- Adjusts spacing to ensure icons are aligned
-                nerd_font_variant = 'mono'
+            keymap = {
+                preset = 'default',
+                ['<Tab>'] = {
+                    function(cmp)
+                        if cmp.snippet_active() then
+                            return cmp.accept()
+                        else
+                            return cmp.select_and_accept()
+                        end
+                    end,
+                    'snippet_forward',
+                    'fallback'
+                },
+                ['<C-b>'] = { 'scroll_documentation_up' },
+                ['<C-f>'] = { 'scroll_documentation_down' },
             },
-
-            -- (Default) Only show the documentation popup when manually triggered
+            appearance = { nerd_font_variant = 'mono' },
             completion = { documentation = { auto_show = true } },
-
-            -- Default list of enabled providers defined so that you can extend it
-            -- elsewhere in your config, without redefining it, due to `opts_extend`
-            sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+            cmdline = {
+                completion = { menu = { auto_show = true } },
+                keymap = { preset = 'inherit' }
             },
-
-            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-            --
-            -- See the fuzzy documentation for more information
-            fuzzy = { implementation = "prefer_rust_with_warning" }
+            signature = {
+                enabled = true,
+                trigger = { enabled = true },
+                window = { border = 'single' },
+            },
         },
         opts_extend = { "sources.default" }
     },
@@ -87,10 +71,137 @@ local plugins = {
         'neovim/nvim-lspconfig',
         dependencies = { 'saghen/blink.cmp' },
 
+        opts = {
+            servers = {
+                -- Lua
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = {
+                                    "vim",
+                                },
+                            },
+                        },
+                    },
+                },
+
+                -- Rust
+                rust_analyzer = {
+                    settings = {
+                        ["rust-analyzer"] = {
+                            imports = {
+                                granularity = {
+                                    group = "module",
+                                },
+                                prefix = "self",
+                            },
+                            cargo = {
+                                buildScripts = {
+                                    enable = true,
+                                },
+                            },
+                            procMacro = {
+                                enable = true
+                            }
+                        }
+                    }
+                },
+
+                -- Terraform
+                terraformls = {},
+
+                -- Python
+                ruff = {
+                    init_options = {
+                        settings = {
+                            args = {
+                                "--extend-select", "E",
+                                "--extend-select", "F",
+                                "--extend-select", "W",
+                                "--extend-select", "I",
+                                "--extend-select", "F401" -- unused imports
+                            }
+                        }
+                    }
+                },
+
+                pyright = { -- Go To Definition capabilities
+                    settings = {
+                        pyright = {
+                            disableOrganizeImports = true,
+                        },
+                        python = {
+                            analysis = {
+                                ignore = { '*' },
+                            },
+                        },
+                    },
+                },
+
+                -- Markdown
+                marksman = {},
+
+                -- Bash
+                bashls = {},
+
+                -- Yaml
+                yamlls = {
+                    on_attach = function(client, bufnr)
+                        local workspace_path = vim.api.nvim_buf_get_name(bufnr)
+                        if workspace_path then
+                            local file_path = vim.fn.expand('%:' .. workspace_path .. ':.')
+                            if string.match(file_path, "helm") then
+                                vim.diagnostic.disable(bufnr)
+                            end
+                        end
+                    end,
+                    settings = {
+                        yaml = {
+                            format = {
+                                enable = true
+                            }
+                        }
+                    }
+                },
+
+                -- Vim
+                vimls = {},
+
+                -- TypeScript
+                ts_ls = {
+                    server_capabilities = {
+                        documentFormattingProvider = false, -- disable ts_ls formatter (using biome instead)
+                    },
+                },
+                biome = {
+                    -- cmd = { "biome", "lsp-proxy" },
+                },
+
+                -- Tailwind CSS
+                tailwindcss = {},
+
+                -- SQL
+                sqlls = {},
+
+                -- JSON
+                jsonls = {},
+
+                -- Helm
+                helm_ls = {},
+            },
+        },
+
         config = function(_, opts)
             local lspconfig = require('lspconfig')
             for server, config in pairs(opts.servers) do
                 config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+
+                -- TODO: move this to the opts config instead
+                if server == "biome" then
+                    config.root_dir = lspconfig.util.root_pattern("package.json", "node_modules", "biome.json")
+                end
+
                 lspconfig[server].setup(config)
             end
         end
