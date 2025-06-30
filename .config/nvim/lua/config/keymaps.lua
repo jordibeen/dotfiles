ee53e77fd27107ef2731939d6d4880ea37c25191
Jordi Been <jordi.been@ridecheck.app>
2025-06-30 02:23:06 +0200

    fix cursor symbol highlighting

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -53,7 +53,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
         bufmap("<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", "Goto Definition in Vertical Split")
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
             bufmap('<leader>th', function()
                 vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
