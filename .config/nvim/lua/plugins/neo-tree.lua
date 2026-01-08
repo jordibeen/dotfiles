4ba33112c345862b4565f872c422bc52d5f6defc
Jordi Been <hi@jordibeen.com>
2026-01-08 01:13:52 +0100

    feat(nvim): add trash command to neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -12,19 +12,6 @@ return {
                 leave_dirs_open = false,
             },
         },
-        filesystem = {
-            filtered_items = {
-                always_show = {
-                    ".gitignore",
-                    ".env",
-                    ".github",
-                    ".test.yaml",
-                    ".production.yaml",
-                    "terraform.tfvars",
-                    ".cargo",
-                }
-            }
-        },
         event_handlers = {
             {
                 event = "file_opened",
@@ -33,16 +20,42 @@ return {
                 end
             },
         },
+        commands = {
+            trash = function(state)
+                local inputs = require("neo-tree.ui.inputs")
+                local path = state.tree:get_node().path
+                local msg = "Are you sure you want to trash " .. path .. "?"
+                inputs.confirm(msg, function(confirmed)
+                    if confirmed then
+                        vim.fn.system({ "mv", vim.fn.fnameescape(path), os.getenv("HOME") .. "/.Trash/" })
+                        require("neo-tree.sources.manager").refresh(state.name)
+                    end
+                end)
+            end,
+
+            trash_visual = function(state, selected_nodes)
+                local inputs = require("neo-tree.ui.inputs")
+                local msg = "Are you sure you want to trash " .. #selected_nodes .. " items?"
+                inputs.confirm(msg, function(confirmed)
+                    if confirmed then
+                        for _, node in ipairs(selected_nodes) do
+                            vim.fn.system({ "mv", vim.fn.fnameescape(node.path), os.getenv("HOME") .. "/.Trash/" })
+                        end
+                        require("neo-tree.sources.manager").refresh(state.name)
+                    end
+                end)
+            end,
+        },
         window = {
             mappings = {
-                ["<bs>"] = "",
                 ["P"] = {
                     "toggle_preview",
                     config = {
                         use_float = false,
                         use_image_nvim = true
                     }
-                }
+                },
+                ["t"] = "trash",
             },
         },
     }
