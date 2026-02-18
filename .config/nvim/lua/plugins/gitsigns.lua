58d656b3023190dde6681d94087f513a7ab4fd08
Jordi Been <hi@jordibeen.com>
2026-02-18 15:48:13 +0100

    chore: register gitsigns keymaps using which-key

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,52 +1,39 @@
 return {
     "lewis6991/gitsigns.nvim",
     opts = {
-        signs                        = {
-            add          = { text = '┃' },
-            change       = { text = '┃' },
-            delete       = { text = '_' },
-            topdelete    = { text = '‾' },
-            changedelete = { text = '~' },
-            untracked    = { text = '┆' },
-        },
-        signs_staged                 = {
-            add          = { text = '┃' },
-            change       = { text = '┃' },
-            delete       = { text = '_' },
-            topdelete    = { text = '‾' },
-            changedelete = { text = '~' },
-            untracked    = { text = '┆' },
-        },
-        signs_staged_enable          = true,
-        signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
-        watch_gitdir                 = {
-            follow_files = true
-        },
-        auto_attach                  = true,
-        attach_to_untracked          = false,
-        current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts      = {
-            virt_text = true,
-            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-            delay = 1000,
-            ignore_whitespace = false,
-            virt_text_priority = 100,
-            use_focus = true,
-        },
-        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-        sign_priority                = 6,
-        update_debounce              = 100,
-        status_formatter             = nil,   -- Use default
-        max_file_length              = 40000, -- Disable if file is longer than this (in lines)
-        preview_config               = {
-            -- Options passed to nvim_open_win
-            style = 'minimal',
-            relative = 'cursor',
-            row = 0,
-            col = 1
-        },
-    }
+        current_line_blame = true
+    },
+    config = function(_, opts)
+        local gitsigns = require('gitsigns')
+        gitsigns.setup(opts)
+        require("which-key").add({
+            {
+                mode = { "n" },
+                nowait = true,
+                remap = false,
+
+                { "<leader>hs", gitsigns.stage_hunk,                                 desc = "Stage hunk" },
+                { "<leader>hr", gitsigns.reset_hunk,                                 desc = "Reset hunk" },
+                { "<leader>hS", gitsigns.stage_buffer,                               desc = "Stage buffer" },
+                { "<leader>hR", gitsigns.reset_buffer,                               desc = "Reset buffer" },
+                { "<leader>hp", gitsigns.preview_hunk,                               desc = "Preview hunk" },
+                { "<leader>hi", gitsigns.preview_hunk_inline,                        desc = "Preview hunk inline" },
+                { "<leader>hb", function() gitsigns.blame_line({ full = true }) end, desc = "Gitsigns blame line full" },
+                { "<leader>hd", gitsigns.diffthis,                                   desc = "Gitsigns diff line" },
+                { "<leader>hD", function() gitsigns.diffthis("~") end,               desc = "Gitsigns diff file" },
+                { "<leader>hQ", function() gitsigns.setqflist('all') end,            desc = "Set all to quickfix list" },
+                { "<leader>hq", gitsigns.setqflist,                                  desc = "Set to quickfix list" },
+                { "<leader>tb", gitsigns.toggle_current_line_blame,                  desc = "Toggle current line blame" },
+                { "<leader>tw", gitsigns.toggle_word_diff,                           desc = "Toggle word diff" },
+            },
+            {
+                mode = { "v" },
+                nowait = true,
+                remap = false,
+
+                { "<leader>hs", function() gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, desc = "Stage hunk" },
+                { "<leader>hr", function() gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, desc = "Reset hunk" },
+            },
+        })
+    end
 }
