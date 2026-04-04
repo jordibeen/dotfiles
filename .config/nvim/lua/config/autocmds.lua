851bdf90ab4635cb7e4819292c4e4433f7b271d6
Jordi Been <hi@jordibeen.com>
2026-04-04 20:42:03 +0200

    fix inlayhinting

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -54,12 +54,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
         if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
             vim.api.nvim_create_autocmd("InsertEnter", {
+                buffer = event.buf,
                 callback = function()
                     vim.lsp.inlay_hint.enable(false, { bufnr = event.buf })
                 end,
             })
 
             vim.api.nvim_create_autocmd("InsertLeave", {
+                buffer = event.buf,
                 callback = function()
                     vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
                 end,
